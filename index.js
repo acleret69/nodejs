aa892cfc888f75cde987d338cc70439a3d1c550d
Aldrick <aldrickcleret@gmail.com>
2020-10-14 09:37:47 +0200

    modif

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
-const { fstat } = require('fs')
 let http = require('http')
+let url = require ("url")
+let fs =require ('fs')
 
 let server = http.createServer()
 
+     const urlObject=url.parse(request.url, true)
+     const fileName = urlObject.pathname
+    console.log(fileName.toString())
+
 server.on('request', (request,response) => {
-    response.writeHead(200 , {
-        'content-type': 'text/html; charset =utf-8'
-    })
+console.log(urlObject.toString())
 
-    response.end('Hello Word')
-
+    if()
+    {
+        console.log('request was made: ' + Request.url);
+        response.writeHead(200, {'Content-Type' : 'application/json'});
+        var myObj = {message : 'Hello world ajax'}        
+    } 
+    else 
+    {
+        response.writeHead(200 , {
+            'content-type': 'text/html; charset =utf-8'
+        })
+        console.log("hello World")
+        response("Hello Word")
+        
+    }
+    response.end();
 })
 server.listen('8080')
 
 
+
+
+// const { fstat } = require('fs')
+// let http = require('http')
+// let url = require('url')
+
+// let server = http.createServer()
+
+// server.on('request', (request,response) => {
+
+//     const urlObject=url.parse(request.url, true)
+//     const fileName = urlObject.pathname
+//     console.log(fileName)
+
+
+//     if (request.fileName = '/ajax'){
+//     response.writeHead(200 , {
+//         'content-type': 'text/html; charset =utf-8'
+//     })
+//     console.log('good')
+
+//     response.end('Hello Word')
+//     }
+
+//     else 
+//      reponse.writeHead(200)
+
+//      console.log('error')
+//      reponse.end('error')
+
+// }) 
+// server.listen('8080');
 
 
     
--- script.js
+++ script.js
@@ -18,12 +18,49 @@ $(function(){
     i=i+1
   });
 
-  //$('#myModal').on('show.bs.modal', function (event) {
-  // var button = $(event.relatedTarget)
-  // var recipient = "Compteur "
-   // var modal = $(this)
-  // modal.find('.modal-body h5').text (recipient+i)
- // })
+  var getHttpRequest = function () {
+    var httpRequest = false;
+  
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+      httpRequest = new XMLHttpRequest();
+      if (httpRequest.overrideMimeType) {
+        httpRequest.overrideMimeType('text/xml');
+      }
+    }
+    else if (window.ActiveXObject) { // IE
+      try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      }
+      catch (e) {
+        try {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch (e) {}
+      }
+    }
+  
+    if (!httpRequest) {
+      alert('Abandon :( Impossible de créer une instance XMLHTTP');
+      return false;
+    }
+  
+    return httpRequest
+  }
+
+  var xhr = getHttpRequest()
+  $('#ajax').on('click',function(){
+
+    xhr.open('GET',"http://localhost:8080/ajax",true)
+    xhr.onreadystatechange=function (){
+     if(xhr.onreadystate === 4){
+        var results = JSON.parse(xhr.reponseText)
+       console.log(results)
+      
+      }
+    }
+    xhr.send()
+  });
+
 
 
 });
